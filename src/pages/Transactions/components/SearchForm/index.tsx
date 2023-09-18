85e38b9c4c488a281b79242dc4151b88b283ecdc
andreapmacedo <aa.andremacedo@gmail.com>
2023-09-18 19:28:38 -0300

    add m03-02

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,6 +4,7 @@ import { SearchFormContainer } from './styles';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useContext } from 'react';
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 
 const searchFormSchema = z.object({
@@ -13,7 +14,13 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext);
+  // const { fetchTransactions } = useContext(TransactionsContext);
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
   const {
     register,
     handleSubmit,
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+// import { useContext } from "react";
+import { useContextSelector } from 'use-context-selector'
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
@@ -8,7 +9,10 @@ import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styl
 
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext)
+  // const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   return (
     <div>
